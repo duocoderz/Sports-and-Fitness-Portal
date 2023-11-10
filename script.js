dbd767d457b6c9500a87e82a72a328f2167692ac
Aryan Sarode <117713349+AryanSarode18@users.noreply.github.com>
2023-11-10 11:42:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll', function(){
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle("sticky", window.scrollY > 50);
 });
+
+const togglebar = document.querySelector('.toggle');
+const menu = document.querySelector('ol');
+
+function navToggle(){
+    togglebar.classList.toggle("active");
+    menu.classList.toggle("active");
+};
